f21fcded4fcfbe804abe289967363a579c515526
Jack Dinsmore <jtd@stanford.edu>
2024-07-26 22:43:48 -0700

    OK

--- peregrine/src/main.rs
+++ peregrine/src/main.rs
@@ -6,6 +6,7 @@ struct Peregrine {
     camera: Camera,
     object: Object,
     exit: bool,
+    size: (u32, u32)
 }
 
 impl App for Peregrine {
@@ -18,15 +19,18 @@ impl App for Peregrine {
         let camera = Camera::new(&graphics, Vector3::new(0., 0., 0.), 1.57, 0., 0.1, 10., 1.5);
         let model = Model::new(graphics, load_obj!("assets/parts/tank.obj"));
         let object = Object::new(graphics, model.clone(), Vector3::new(0., 0., 0.), Quaternion::new(1., 0., 0., 0.));
+        let size = graphics.get_size();
         Self {
             exit: false,
             shader,
             object,
             camera,
+            size,
         }
     }
 
-    fn tick(&mut self, key_state: &KeyState) {
+    fn tick(&mut self, graphics: &Graphics, key_state: &KeyState) {
+        graphics.set_mouse_pos((self.size.0/2, self.size.1/2));
         if key_state.is_down(Key::Char('w')) {
             self.camera.position.x += 0.01;
         }
@@ -36,11 +40,11 @@ impl App for Peregrine {
         }
 
         if key_state.is_down(Key::Char('a')) {
-            self.camera.position.y -= 0.01;
+            self.camera.position.y += 0.01;
         }
 
         if key_state.is_down(Key::Char('d')) {
-            self.camera.position.y += 0.01;
+            self.camera.position.y -= 0.01;
         }
 
         if key_state.is_down(Key::Char('q')) {
@@ -65,6 +69,13 @@ impl App for Peregrine {
             Key::Escape => self.exit = true,
             Key::Char(_) => (),
         }
+    }
+
+    fn mouse_motion(&mut self, pos: (f64, f64)) {
+        let dx = (pos.0 - self.size.0 as f64 / 2.) / 300.;
+        let dy = (pos.1 - self.size.1 as f64 / 2.) / 300.;
+        self.camera.phi += -dx as f32;
+        self.camera.theta += dy as f32;
     }
     
     fn render(&self, render_pass: RenderPass) {
